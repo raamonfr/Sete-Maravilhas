32b2a51bca0759eb4153ad4d381eb4f048f25e90
LAB <lab@unibra.ub>
2024-05-30 09:44:26 -0300

    initializing the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'damion': ['Damion', 'cursive'],
+        'icones': ['Icones']
+      }
+    },
   },
   plugins: [],
 }
